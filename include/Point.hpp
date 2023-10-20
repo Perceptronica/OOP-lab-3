1854094e6297ecc8a6241d45a911c4e7391afb6d
Perceptronica <klvlwxxw@ya.ru>
2023-10-20 20:25:59 +0300

    commiting a crime against C++

---
+++ include/Point.hpp
@@ -0,0 +1,48 @@
+// (c) anton perceptronica, 2023
+
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+#ifndef POINT_HPP
+#define POINT_HPP
+
+class Point {
+private:
+    double x;
+    double y;
+
+public:
+    Point(): x(0.0), y(0.0) {};
+    Point(double x_, double y_): x(x_), y(y_) {};
+    
+    Point(const Point& other): x(other.x), y(other.y) {};
+    Point(Point&& other) noexcept; 
+    Point& operator=(const Point& other);
+    Point& operator=(Point&& other) noexcept;
+
+    ~Point() = default;
+
+    double getX() const { return x; }
+    double getY() const { return y; }
+    double length() const; // так как точка ≃ радиус-вектор
+
+    Point operator+(const Point& other) const;
+    Point operator-(const Point& other) const;
+};
+
+#endif
+
+/*
+- конструктор по умолчанию
+- конструктор с параметрами
+- конструктор копирования (1)
+- конструктор перемещения (2)
+- оператор присваивания копированием (3)
+- оператор присваивания перемещением (4)
+- деструктор (5)
+*/
---
+++ src/Point.cpp
@@ -0,0 +1,30 @@
+// (c) anton perceptronica, 2023
+
+#include "Point.hpp"
+
+Point::Point(Point&& other) noexcept {
+    x = other.x;
+    y = other.y;
+    other.x = 0;
+    other.y = 0;
+}
+
+Point& Point::operator=(const Point& other) {
+    if (this == &other) return *this;
+    x = other.x;
+    y = other.y;
+    return *this;
+}
+
+Point& Point::operator=(Point&& other) noexcept {
+    if (this == &other) return *this;
+    x = other.x;
+    y = other.y;
+    other.x = 0;
+    other.y = 0;
+    return *this;
+}
+
+double Point::length() const {
+    return sqrt(getX() * getX() + getY() * getY());
+}
